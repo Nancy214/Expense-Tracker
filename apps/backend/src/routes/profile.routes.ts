ede5b1d297c5f63740a41c9c30e3437720193335
nancy <nancy214patel@gmail.com>
2025-10-17 16:56:28 +0400

    added validate middleware for profile

--- apps/backend/src/routes/profile.routes.ts
+++ apps/backend/src/routes/profile.routes.ts
@@ -9,6 +9,8 @@ import {
 } from "../controllers/profile.controller";
 import { authenticateToken } from "../middleware/auth.middleware";
 import { upload } from "../config/multer";
+import { validate } from "../middleware/validate.middleware";
+import { ZProfileData, ZSettingsData } from "@expense-tracker/shared-types/src/profile";
 
 const router = express.Router();
 
@@ -22,7 +24,13 @@ router.get("/country-timezone-currency", getCountryTimezoneCurrency as RequestHa
 router.get("/:userId", authenticateToken as RequestHandler, getProfile as RequestHandler);
 
 // Update profile information
-router.put("/", authenticateToken as RequestHandler, upload.single("profilePicture"), updateProfile as RequestHandler);
+router.put(
+    "/",
+    authenticateToken as RequestHandler,
+    upload.single("profilePicture"),
+    validate(ZProfileData, "body"),
+    updateProfile as RequestHandler
+);
 
 // Upload profile picture
 /* router.post(
@@ -33,7 +41,12 @@ router.put("/", authenticateToken as RequestHandler, upload.single("profilePictu
 );
  */
 // Update user settings
-router.put("/settings", authenticateToken as RequestHandler, updateSettings as RequestHandler);
+router.put(
+    "/settings",
+    authenticateToken as RequestHandler,
+    validate(ZSettingsData, "body"),
+    updateSettings as RequestHandler
+);
 
 // Delete profile picture
 router.delete("/picture", authenticateToken as RequestHandler, deleteProfilePicture as RequestHandler);
