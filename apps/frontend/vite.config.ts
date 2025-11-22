e81e97207759a38a42a8e825870bbf423abc1958
nancy <nancy214patel@gmail.com>
2025-11-22 17:16:45 +0400

    update for pnpm

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -3,13 +3,14 @@ import path from "path";
 import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-	},
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+    root: __dirname,
+    plugins: [react()],
+    server: {
+        port: 3000,
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 });
